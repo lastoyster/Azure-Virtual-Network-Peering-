ee7c86bae93a521377d134ee00397f02657e2d1d
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2025-08-25 22:34:03 +0530

    added security_groups

---
+++ security_group.tf
@@ -0,0 +1,61 @@
+# VNet A
+resource "azurerm_virtual_network" "vnet_a" {
+  name                = "VNetA"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_a" {
+  name                 = "SubnetA"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_a.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+
+resource "azurerm_virtual_network" "vnet_b" {
+  name                = "VNetB"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_b" {
+  name                 = "SubnetB"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_b.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+# NSG for VNet A
+resource "azurerm_network_security_group" "nsg_a" {
+  name                = "NSG-A"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH-From-VNetB"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.1.0.0/16"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-ICMP-From-VNetB"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.1.0.0/16"
+    destination_address_prefix = "*"
+  }
+}
